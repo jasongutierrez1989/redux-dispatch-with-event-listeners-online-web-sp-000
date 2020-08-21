4abff9fce22536a8b1b983c4d896297d3444e527
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-08-21 04:03:56 +0000

    Automatically backed up by Learn

--- js/reducer.js
+++ js/reducer.js
@@ -1,10 +1,22 @@
-let state = {count: 0}
-
-function reducer(state, action){
+let state;
+ 
+function reducer(state = {count: 0}, action){
   switch (action.type) {
     case 'INCREASE_COUNT':
-      return {count: state.count + 1};
+      return {count: state.count + 1}
     default:
       return state;
   }
 }
+ 
+function dispatch(action){
+  state = reducer(state, action)
+  render()
+}
+ 
+function render(){
+  let container = document.getElementById('container');
+  container.textContent = state.count;
+}
+ 
+dispatch({type: '@@INIT'})
